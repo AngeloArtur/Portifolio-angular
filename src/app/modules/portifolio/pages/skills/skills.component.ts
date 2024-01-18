89f9b981b608ca7a789113ba72d8860da0315d6c
Ângelo Artur <ang.vianna@gmail.com>
2024-01-18 19:02:02 -0300

    skills component

--- src/app/modules/portifolio/pages/skills/skills.component.ts
+++ src/app/modules/portifolio/pages/skills/skills.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { ISkills } from '../../interface/ISkills.interface';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  public arraySkills = signal<ISkills[]>([
+    {
+      src: 'assets/icons/html.svg',
+      nameSkill: 'html',
+      alt: 'Ícone de conhecimento em html'
+    },
+    {
+      src: 'assets/icons/css.svg',
+      nameSkill: 'css',
+      alt: 'Ícone de conhecimento em css'
+    },
+    {
+      src: 'assets/icons/javascript.svg',
+      nameSkill: 'javascript',
+      alt: 'Ícone de conhecimento em javascript'
+    },
+    {
+      src: 'assets/icons/angular.svg',
+      nameSkill: 'angular',
+      alt: 'Ícone de conhecimento em angular'
+    },
+    {
+      src: 'assets/icons/java.svg',
+      nameSkill: 'java',
+      alt: 'Ícone de conhecimento em java'
+    },
+    {
+      src: 'assets/icons/spring-boot.svg',
+      nameSkill: 'spring-boot',
+      alt: 'Ícone de conhecimento em spring-boot'
+    },
+  ])
 }
