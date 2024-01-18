f88b2ca36f09a55c6e6d231f62f4cac21dd14748
Ângelo Artur <ang.vianna@gmail.com>
2024-01-18 19:00:02 -0300

    Interface Type

---
+++ src/app/modules/portifolio/interface/IProject.interface.ts
@@ -0,0 +1,11 @@
+export interface IProject {
+    src: string,
+    alt: string,
+    width: string,
+    height: string,
+    description: string,
+    links: {
+      name: string,
+      href: string
+    }
+}
---
+++ src/app/modules/portifolio/interface/ISkills.interface.ts
@@ -0,0 +1,5 @@
+export interface ISkills {
+    src: string,
+    nameSkill:string,
+    alt: string
+}
