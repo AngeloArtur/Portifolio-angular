f43d7657ead83bd98dd821eb9639f0c5f22d1e9d
Ângelo Artur <ang.vianna@gmail.com>
2024-01-17 21:13:28 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './modules/portifolio/components/header/header.component';
+import { NavbarComponent } from './modules/portifolio/components/navbar/navbar.component';
+import { SkillsComponent } from './modules/portifolio/pages/skills/skills.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent,
+    NavbarComponent,
+    SkillsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
