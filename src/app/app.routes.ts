2919552348224f1fca8f46ee34d542527ba82ea4
Ângelo Artur <ang.vianna@gmail.com>
2024-02-12 15:34:15 -0300

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/portifolio/pages/home/home.component';
+import { SkillsComponent } from './modules/portifolio/pages/skills/skills.component';
+import { ExperiencesComponent } from './modules/portifolio/pages/experiences/experiences.component';
+import { PortfolioComponent } from './modules/portifolio/pages/portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomeComponent },
+    { path:'skills', component: SkillsComponent },
+    { path:'experiences', component: ExperiencesComponent },
+    { path: 'portfolio', component:PortfolioComponent }
+];
